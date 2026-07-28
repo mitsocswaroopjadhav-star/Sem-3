b623bcf48ad3b76d0ef3cf93566312103c744ee3
Swaroop Jadhav <mitsocswaroopjadhav@gmail.com>
2026-07-28 12:50:20 +0530

    Add files via upload

---
+++ Assignment3 .py
@@ -0,0 +1,48 @@
+from abc import ABC, abstractmethod
+
+
+class Payment(ABC):
+    @abstractmethod
+    def pay(self, amount):
+        pass
+
+class UPI(Payment):
+    def pay(self, amount):
+        print("Paid", amount, "using UPI")
+
+class Card(Payment):
+    def pay(self, amount):
+        print("Paid", amount, "using Card")
+
+class NetBanking(Payment):
+    def pay(self, amount):
+        print("Paid", amount, "using Net Banking")
+
+
+class PaymentProcessor:
+    def set_payment(self, payment):
+        self.payment = payment
+
+    def process(self, amount):
+        self.payment.pay(amount)
+
+processor = PaymentProcessor()
+
+print("1. UPI")
+print("2. Card")
+print("3. Net Banking")
+
+choice = int(input("Enter choice: "))
+amount = int(input("Enter amount: "))
+
+if choice == 1:
+    processor.set_payment(UPI())
+elif choice == 2:
+    processor.set_payment(Card())
+elif choice == 3:
+    processor.set_payment(NetBanking())
+else:
+    print("Invalid Choice")
+    exit()
+
+processor.process(amount)
